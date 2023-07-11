1607ddaca38f61769a517ddeda00aac9a35a9567
bobbyrabia <rpfreire1@espe.edu.ec>
2023-07-11 12:50:37 -0500

    initial configuration start init

---
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/LoanApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.banquito.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplication.class, args);
+	}
+
+}
