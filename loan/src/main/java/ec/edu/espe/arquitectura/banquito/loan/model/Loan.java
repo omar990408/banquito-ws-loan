315551418e4e6a2022048f017283081e868844cf
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-12 18:28:56 -0500

    update

---
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/Loan.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.arquitectura.banquito.loan.model;
+
+import java.sql.Date;
+
+public class Loan {
+
+    private String loanId;
+    private String sccountHolderType;
+    private Date crationDate;
+    private Date aproverDate;
+    private Date lastModified;
+    
+    
+}
