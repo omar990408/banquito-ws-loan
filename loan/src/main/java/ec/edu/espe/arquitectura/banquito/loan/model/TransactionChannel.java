fd751d07cb5690178b14b884a082cefb0c197455
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-13 05:15:47 -0500

    update

---
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/TransactionChannel.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.arquitectura.banquito.loan.model;
+
+import java.sql.Date;
+
+public class TransactionChannel {
+    private Integer Id;
+    private String name;
+    private Date activated;
+    private String loanConstrainsUsage;
+    private Integer version; 
+
+}
