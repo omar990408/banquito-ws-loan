af22c8cf301021daefca071b05a0fc0a57ce8e3a
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-12 23:26:31 -0500

    update

--- loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/Loan.java
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/Loan.java
@@ -1,5 +1,6 @@
 package ec.edu.espe.arquitectura.banquito.loan.model;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 
 public class Loan {
@@ -9,6 +10,33 @@ public class Loan {
     private Date crationDate;
     private Date aproverDate;
     private Date lastModified;
+    private String name;
+    private Integer amount;
+    private Integer principalDue;
+    private Integer interestPaid;
+    private Integer feesDue;
+    private Integer feesPaid;
+    private Integer penaltyDue;
+    private Integer penaltyPaid;
+    private Integer repaymentPeriodCount;
+    private String reapaymentPeriodUnit;
+    private Integer repaymentInstallmenst;
+    private String gracePeriodType;
+    private Integer gracePeriod;
+    private BigDecimal interestRate;
+    private String interestChargeFrequence;
+    private String interestCalculationMethod;
+    private String notes;
+    private Date lastAccountAppraisalDate;
+    private String scheuldeDueDatesMethod;
+    private Integer fixedDays;
+    private BigDecimal taxRate;
+    private BigDecimal penaltyRate;
+    private BigDecimal principalPaid;
+    private Integer version;
+
+
+
     
     
 }
