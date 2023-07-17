f46e4477860c9a8da2c6a567f2efcecb14da35de
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-17 18:08:32 -0500

    manage loan

--- loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/controller/LoanController.java
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/controller/LoanController.java
@@ -1,6 +1,8 @@
 package ec.edu.espe.arquitectura.banquito.loan.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,23 +16,55 @@ import ec.edu.espe.arquitectura.banquito.loan.service.LoanService;
 @RequestMapping
 public class LoanController {
 
-
+    @Autowired
     private final LoanService loanService;
 
 
     public LoanController(LoanService loanService) {
+
         this.loanService = loanService;
     }
 
-    @PostMapping
+    @PostMapping("/loan")
+    @ResponseBody
     public ResponseEntity<Loan> createLoan(@RequestBody Loan loan) {
-        Loan createdLoan = loanService.createLoan(loan);
+        Loan createdLoan = loanService.loanCreate(loan);
         return new ResponseEntity<>(createdLoan, HttpStatus.CREATED);
     }
-    
-    @GetMapping("/ruta")
-    public List<Loan> obtainLoansByClientId(@PathVariable Integer clientId) {
-        return loanService.obtainfindByClientId(clientId);
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Loan> obtainById(@PathVariable(name = "id") Integer id) {
+        Optional<Loan> loan = this.loanService.obtainById(id);
+        if (loan.isPresent()) {
+            return ResponseEntity.ok(loan.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
     
+
+
+    @PutMapping
+    public ResponseEntity <Loan>update(@RequestBody Loan loan) {
+        try {
+            this.loanService.updateLoan(loan);
+            return ResponseEntity.ok(loan);
+        } catch (RuntimeException rte) {
+            throw new NoSuchElementException("Prestamo: " + loan.getId() + " No existe");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Loan> delete(@PathVariable(name = "id") Integer id) {
+
+        try {
+            this.loanService.delete(id);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException rte) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+
 }
--- loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/service/LoanService.java
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/service/LoanService.java
@@ -1,7 +1,9 @@
 package ec.edu.espe.arquitectura.banquito.loan.service;
 
-import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 
 import ec.edu.espe.arquitectura.banquito.loan.model.Loan;
@@ -15,39 +17,97 @@ import org.springframework.stereotype.Service;
 public class LoanService {
 
     private final LoanRepository loanRepository;
-   // private final LoanTransactionRepository loanTransactionRepository;
-    //private final AmortizationRepository amortizationRepository;
+    private final LoanTransactionRepository loanTransactionRepository;
+    private final AmortizationRepository amortizationRepository;
 
 
-    public LoanService(LoanRepository loanRepository) {
+    public LoanService(LoanRepository loanRepository, LoanTransactionRepository loanTransactionRepository, AmortizationRepository amortizationRepository) {
         this.loanRepository = loanRepository;
-       // this.loanTransactionRepository = loanTransactionRepository;
-        //this.amortizationRepository = amortizationRepository;
+        this.loanTransactionRepository = loanTransactionRepository;
+        this.amortizationRepository = amortizationRepository;
     }
 
-    public List<Loan> obtainfindByClientId(Integer clientId) {
-        return loanRepository.findByClientId(clientId);
+
+
+    public Optional<Loan> obtainById(Integer id) {
+        Optional<Loan> loanOpt = this.loanRepository.findById(id);
+        if (loanOpt.isPresent()) {
+            return loanOpt;
+        } else {
+            throw new RuntimeException("Prestamo con ID: " + id + " no encontrado");
+        }
     }
 
-    public Loan getLoanById(Integer id) {
-        return loanRepository.findById(id).orElse(null);
-    }
 
+   /* @Transactional
     public Loan createLoan(Loan loan) {
         BigDecimal interestRate = loan.getAmount().compareTo(new BigDecimal(1000)) == 0 ? new BigDecimal("0.05") : new BigDecimal("0.03");
         BigDecimal interestAmount = loan.getAmount().multiply(interestRate);
         loan.setInterestDue(interestAmount);
         return loanRepository.save(loan);
-    }
+    }*/
+   @Transactional
+    public Loan loanCreate(Loan loan) {
 
+       //Loan loanTmp = this.loanRepository.findByLoanClientIdAndProductId(loan, loan.getLoanProductId());
+       Optional<Loan> loanTmp = loanRepository.findById(loan.getLoanProductId());
+      if(loanTmp!=null){
+       loan.setCreationDate(new Date());
+       loan.setLastModifiedDate(new Date());
+       loan.setApprovedDate(new Date());
+       loan.setInterestCalculationMethod("FLA");
+       loan.setState("ACT");
+
+           return this.loanRepository.save(loan);
+       } else {
+           throw new RuntimeException("Prestamo con ID " + loan.getId() + " ya existe");
+       }
+
+   }
     public Loan updateLoan(Loan loan) {
         
         return loanRepository.save(loan);
     }
 
-    public void deleteLoan(Integer id) {
-        loanRepository.deleteById(id);
+
+
+
+
+    @Transactional
+    public Loan delete(Integer loanId) {
+        try {
+            Optional<Loan> loanOpt = this.loanRepository.findById(loanId);
+            if (loanOpt.isPresent()) {
+                Loan loanTmp = loanOpt.get();
+                loanTmp.setState("INA");
+                loanTmp.setLastModifiedDate(new Date());
+                return this.loanRepository.save(loanTmp);
+            } else {throw new RuntimeException("El prestamo: " + loanId+"no existe");
+            }} catch (RuntimeException rte)
+        {
+            throw new RuntimeException("No se puede eliminar el prestamo: " + loanId, rte);
+        }
+    }
+
+
+
+
+
+    public String  loanSimulation(Integer mountSimulation, Integer mounths) {
+        double calcutation, total;
+        double interesRate = 0.156;
+        double pagoMensual = 0;
+        if (mountSimulation != null && mounths != null) {
+            calcutation = mountSimulation * interesRate;
+            total = mountSimulation + interesRate;
+            pagoMensual = total / mounths;
+
+        }
+        return "Retornar valores simulados o hacer en el front";
+
+
     }
     
     }
+
 
