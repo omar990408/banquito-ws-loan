3f14d21e52c1ce16a5514044156ea0196ad4b2cc
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-17 18:08:32 -0500

    manage loan

--- loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/Loan.java
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/model/Loan.java
@@ -2,9 +2,7 @@ package ec.edu.espe.arquitectura.banquito.loan.model;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
--- loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/repository/LoanRepository.java
+++ loan/src/main/java/ec/edu/espe/arquitectura/banquito/loan/repository/LoanRepository.java
@@ -3,13 +3,16 @@ package ec.edu.espe.arquitectura.banquito.loan.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ec.edu.espe.arquitectura.banquito.loan.model.Loan;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
 
-    List<Loan> findByClientId(Integer clientId);
+
+
+
 
 }
     
